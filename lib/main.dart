98e8e0e676ab72357eeb8d7a6b7ee656ac6074b4
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-11-03 13:02:44 -0500

    working on authentication

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,28 @@
 //
 
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import "package:flutter/material.dart";
 import 'package:provider/provider.dart';
+import 'package:tangled/amplifyconfiguration.dart';
 import 'package:tangled/services/authService.dart';
 
 import 'authWrapper.dart';
 
-void main(List<String> args) {
+_configureAmplify() async {
+  try {
+    final auth = AmplifyAuthCognito();
+    await Amplify.addPlugins([auth]);
+    await Amplify.configure(amplifyconfig);
+  } catch (error) {
+    safePrint("Couldn't configure amplify $error");
+  }
+}
+
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await _configureAmplify();
   runApp(TangledApp());
 }
 
