d3d22f5662bfc2164d390f8c6fda721de149e4ca
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-11-04 10:49:24 -0500

    authScreen

--- lib/auth/authScreen.dart
+++ lib/auth/authScreen.dart
@@ -7,19 +7,67 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Tangled",
-                style: TextStyle(
-                  fontSize: 32,
-                  fontWeight: FontWeight.bold,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Tangled",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-            ],
+                SizedBox(height: 28),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text(
+                    "Sign in",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xffFF0054),
+                    ),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      Size(double.infinity, 50),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 8),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text("Create an account"),
+                  style: ButtonStyle(
+                    foregroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xff312F2F),
+                    ),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xffEDF2F4),
+                    ),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      Size(double.infinity, 50),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
