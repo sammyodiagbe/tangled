5594c5726328d4742ec7aea9b3e8d3279d713cc3
Odiagbe Samson <sam.odiagbe.dev@gmail.com>
2022-11-08 10:13:31 -0600

    signing user up with phone numbers

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -2,8 +2,27 @@
 import "package:flutter/material.dart";
 import 'package:intl_phone_number_input/intl_phone_number_input.dart';
 
-class Signup extends StatelessWidget {
+class Signup extends StatefulWidget {
   static final id = 'Signup';
+
+  @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  final _formKey = GlobalKey<FormState>();
+  final _usernameController = TextEditingController();
+  final _mobileController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _usernameController.dispose();
+    _mobileController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,47 +31,63 @@ class Signup extends StatelessWidget {
       child: Center(
         child: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Tangled",
-                style: TextStyle(
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold,
-                  color: Color(0xff312F2F),
-                ),
-              ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                "After providing your phone number, we would send you an OTP to verify.",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  color: Color(0xff312F2F),
-                ),
-              ),
-              SizedBox(height: 20),
-              TextField(
-                decoration: InputDecoration(
-                  hintText: "What should we call you?",
-                  hintStyle: TextStyle(fontSize: 14),
-                  filled: true,
-                  fillColor: Color(0xffEDF2F4),
-                  enabledBorder: OutlineInputBorder(
-                    borderSide: BorderSide.none,
-                    borderRadius: BorderRadius.circular(8),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Tangled",
+                  style: TextStyle(
+                    fontSize: 40,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff312F2F),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              InternationalPhoneNumberInput(
-                onInputChanged: (value) {},
-                spaceBetweenSelectorAndTextField: 5,
-                inputDecoration: InputDecoration(
+                SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  "After providing your phone number, we would send you an OTP to verify.",
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Color(0xff312F2F),
+                  ),
+                ),
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _usernameController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "We need a name to call you.";
+                    }
+                  },
+                  decoration: InputDecoration(
+                    hintText: "What should we call you?",
+                    hintStyle: TextStyle(fontSize: 14),
+                    filled: true,
+                    fillColor: Color(0xffEDF2F4),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                InternationalPhoneNumberInput(
+                  onInputChanged: (value) {
+                    String? number = value.phoneNumber;
+                    _mobileController.text = number ?? "";
+                  },
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Phone number is required";
+                    }
+                  },
+                  spaceBetweenSelectorAndTextField: 5,
+                  inputDecoration: InputDecoration(
                     filled: true,
                     fillColor: Color(0xffEDF2F4),
                     hintStyle: TextStyle(fontSize: 14),
@@ -60,35 +95,68 @@ class Signup extends StatelessWidget {
                     enabledBorder: OutlineInputBorder(
                       borderSide: BorderSide.none,
                       borderRadius: BorderRadius.circular(8.0),
-                    )),
-              ),
-              SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Create account",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
-                style: ButtonStyle(
-                  elevation: MaterialStateProperty.all<double>(0),
-                  foregroundColor:
-                      MaterialStateProperty.all<Color>(Colors.white),
-                  backgroundColor: MaterialStateProperty.all<Color>(
-                    Color(0xffFF0054),
-                  ),
-                  minimumSize: MaterialStateProperty.all<Size>(
-                    Size(double.infinity, 50),
-                  ),
-                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                    RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(24),
                     ),
                   ),
                 ),
-              ),
-            ],
+                SizedBox(height: 20),
+                TextFormField(
+                  controller: _passwordController,
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Password is required.";
+                    }
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Password",
+                    hintStyle: TextStyle(fontSize: 14),
+                    filled: true,
+                    fillColor: Color(0xffEDF2F4),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide.none,
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: () {
+                    print(_passwordController.value.text);
+                    print(_usernameController.value.text);
+                    print(_mobileController.value.text);
+                    if (_formKey.currentState!.validate()) {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        SnackBar(
+                          content: Text("Valid data"),
+                        ),
+                      );
+                    }
+                  },
+                  child: Text(
+                    "Create account",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    elevation: MaterialStateProperty.all<double>(0),
+                    foregroundColor:
+                        MaterialStateProperty.all<Color>(Colors.white),
+                    backgroundColor: MaterialStateProperty.all<Color>(
+                      Color(0xffFF0054),
+                    ),
+                    minimumSize: MaterialStateProperty.all<Size>(
+                      Size(double.infinity, 50),
+                    ),
+                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/services/authService.dart
+++ lib/services/authService.dart
@@ -1,9 +1,26 @@
+import 'package:amplify_auth_cognito/amplify_auth_cognito.dart';
+import 'package:amplify_flutter/amplify_flutter.dart';
 import 'package:flutter/foundation.dart';
 
 enum AuthState { Authenticated, NotAuthenticated }
 
 class AuthService extends ChangeNotifier {
   var authState = AuthState.NotAuthenticated;
+
+  Future<void> signUpWithMobile(
+      String name, String mobile, String password) async {
+    try {
+      await Amplify.Auth.signUp(
+        username: name,
+        password: password,
+        options: CognitoSignUpOptions(
+          userAttributes: <CognitoUserAttributeKey, String>{
+            CognitoUserAttributeKey.phoneNumber: mobile
+          },
+        ),
+      );
+    } catch (err) {}
+  }
 
   void SignUserIn() {
     authState = AuthState.Authenticated;
